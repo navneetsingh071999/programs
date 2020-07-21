fe7dbadf5d59ebc0e5bbe42ef58e0890d23665e0
navneetsingh071999 <navneet.singh071999@gmail.com>
2020-07-21 15:49:07 +0530

    Dijkstras algorithm using c++ map

---
+++ dijkstras_using_map.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+map<int, map<int,int> > read(int , map<int, map<int, int> > );
+map<int, int> dijkstras(int, int, map<int, map<int, int> > , map<int, int>);
+
+
+int minm(int a, int b)
+{
+	return ((a>b)?b:a);
+}
+
+int main()
+{
+	int n, source, dest;
+	cout<<"Enter no of vertices: ";
+	cin>>n;
+	cout<<"\nEnter Source Node: ";
+	cin>>source;
+	
+	map <int, int> dist;
+	map <int, map<int,int> > cost;
+	
+	cout<<"\nEnter Cost Matrix: \n";
+	cost = read(n, cost);
+
+	dist = dijkstras(n, source, cost, dist);
+	//cout<<"\nEnter Destination: ";
+	//cin>>dest;
+	cout<<"\n";
+	
+	map<int, int> :: iterator itr;
+	cout<<"Source	 | Destination 	|  Distance\n";
+	for(itr = dist.begin(); itr != dist.end(); ++itr)
+	{
+		cout<<source <<" 	 |	"<<itr->first << "    	| 	"<< itr->second<<"\n";	
+	}
+	
+	//cout<<"\nDistance from "<<source<<" to "<<dest<<" is: "<<dist[dest];
+	return 0;
+}
+
+map<int, map<int,int> > read(int n, map<int, map<int, int> > cost)
+{
+	int value;
+	for(int i = 1; i <= n; i++)
+	{
+		for(int j = 1; j <= n; j++)
+		{
+			cin>>value;
+			cost[i][j] = value;
+		}
+	}
+	return cost;
+}
+
+map <int, int> dijkstras(int n, int source, map<int, map<int, int> > cost, map<int, int> dist)
+{
+	int w, min;
+	map<int, int> visited;
+	for(int i = 1; i <= n; i++)
+	{
+		visited[i] = 0;
+		dist[i] = cost[source][i];
+		
+	} 
+	
+	visited[source] = 1;
+	
+	for(int i = 1; i <= n-1 ; i++)
+	{
+		min = 9999;
+		for(int j = 1; j <= n; j++)
+		{
+			if((visited[j] == 0) && (min > dist[j]))
+			{
+				min = dist[j];
+				w = j;
+			}
+		}
+		
+		visited[w] = 1;
+		for(int v = 1; v <= n; v++)
+		{
+			if(visited[v] == 0)
+				dist[v] = minm(dist[v],cost[w][v]+dist[w]);
+		}
+	}
+	
+	return dist;
+}
+
